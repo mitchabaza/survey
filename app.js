3381cb8ccb8175a373d0f6601cf3f8ceeb3e4689
Mitch Abaza <mitch.abaza@rph.com>
2016-05-10 11:36:20 -0500

    leveldb implementation

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var env = require('node-env-file');
 
 var app = express();
 
-// if in development mode, load .env variables
-if (app.get("env") === "development") {
-    env(__dirname + '/.env');
-}
 
-// connect to database
-app.db = mongoose.connect(process.env.MONGOLAB_URI);
 
 // view engine setup - this app uses Hogan-Express
 // https://github.com/vol4ok/hogan-express
@@ -56,16 +49,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
-});
-
+ 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-
+var util = require("util")
 // our db model
-var Animal = require("../models/model.js");
+var levelup = require('levelup')
+
+// 1) Create our database, supply location and options.
+//    This will create or open the underlying LevelDB store.
+
+
+var db = levelup('./survey', {valueEncoding:'json'})
+var Models = require('level-orm');
+
+function Questions(db) {
+    // users is the sublevel name to user
+    // handle is the primary key to user for insertion
+    Models.call(this, { db: db }, 'questions', 'handle');
+}
+util.inherits(Questions, Models);
+
+var questions = new Questions(db);
 
 /**
  * GET '/'
@@ -13,13 +28,11 @@ var Animal = require("../models/model.js");
  */
 router.get('/', function(req, res) {
   
-  var jsonData = {
-  	'name': 'node-express-api-boilerplate',
-  	'api-status':'OK'
-  }
 
   // respond with json data
-  res.json(jsonData)
+  res.json({status:
+      "OK"
+  })
 });
 
 // simple route to show an HTML page
@@ -38,51 +51,27 @@ router.post('/api/create', function(req, res){
 
     console.log(req.body);
 
-    // pull out the information from the req.body
-    var name = req.body.name;
-    var age = req.body.age;
-    var tags = req.body.tags.split(","); // split string into array
-    var weight = req.body.weight;
-    var color = req.body.color;
-    var url = req.body.url;
+     questions.save(req.body,
+        function (err, id) {
+            // id will be the primary key
+        });
 
-    // hold all this data in an object
-    // this object should be structured the same way as your db model
-    var animalObj = {
-      name: name,
-      age: age,
-      tags: tags,
-      description: {
-        weight: weight,
-        color: color
-      },
-      url: url
-    };
+    questions.get()
+    return;
+// 2) put a key & value
+        db.put('surveyanswer',req.body, function (err) {
+        if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-    // create a new animal model instance, passing in the object
-    var animal = new Animal(animalObj);
+        // 3) fetch by key
+        db.get('surveyanswer', function (err, value) {
+            if (err) return console.log('Ooops!', err) // likely the key was not found
 
-    // now, save that animal instance to the database
-    // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save    
-    animal.save(function(err,data){
-      // if err saving, respond back with error
-      if (err){
-        var error = {status:'ERROR', message: 'Error saving animal'};
-        return res.json(error);
-      }
+            // ta da!
+            return res.json(value);
+        })
+    })
 
-      console.log('saved a new animal!');
-      console.log(data);
-
-      // now return the json data of the new animal
-      var jsonData = {
-        status: 'OK',
-        animal: data
-      }
-
-      return res.json(jsonData);
-
-    })  
+    // })
 });
 
 // /**
@@ -124,24 +113,12 @@ router.get('/api/get/:id', function(req, res){
 
 router.get('/api/get', function(req, res){
 
-  // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
-  Animal.find(function(err, data){
-    // if err or no animals found, respond with error 
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find animals'};
-      return res.json(error);
-    }
-
-    // otherwise, respond with the data 
-
-    var jsonData = {
-      status: 'OK',
-      animals: data
-    } 
-
-    res.json(jsonData);
-
-  })
+    db.get('surveyanswer', function (err, value) {
+        if (err) return console.log('Ooops!', err) // likely the key was not found
+        console.log(JSON.stringify(value))
+        // ta da!
+        return res.json(value);
+    })
 
 })
 
@@ -153,107 +130,5 @@ router.get('/api/get', function(req, res){
 //  * @return {Object} JSON
 //  */
 
-router.post('/api/update/:id', function(req, res){
-
-   var requestedId = req.param('id');
-
-   var dataToUpdate = {}; // a blank object of data to update
-
-    // pull out the information from the req.body and add it to the object to update
-    var name, age, weight, color, url; 
-
-    // we only want to update any field if it actually is contained within the req.body
-    // otherwise, leave it alone.
-    if(req.body.name) {
-      name = req.body.name;
-      // add to object that holds updated data
-      dataToUpdate['name'] = name;
-    }
-    if(req.body.age) {
-      age = req.body.age;
-      // add to object that holds updated data
-      dataToUpdate['age'] = age;
-    }
-    if(req.body.weight) {
-      weight = req.body.weight;
-      // add to object that holds updated data
-      dataToUpdate['description'] = {};
-      dataToUpdate['description']['weight'] = weight;
-    }
-    if(req.body.color) {
-      color = req.body.color;
-      // add to object that holds updated data
-      if(!dataToUpdate['description']) dataToUpdate['description'] = {};
-      dataToUpdate['description']['color'] = color;
-    }
-    if(req.body.url) {
-      url = req.body.url;
-      // add to object that holds updated data
-      dataToUpdate['url'] = url;
-    }
-
-    var tags = []; // blank array to hold tags
-    if(req.body.tags){
-      tags = req.body.tags.split(","); // split string into array
-      // add to object that holds updated data
-      dataToUpdate['tags'] = tags;
-    }
-
-
-    console.log('the data to update is ' + JSON.stringify(dataToUpdate));
-
-    // now, update that animal
-    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
-    Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
-      // if err saving, respond back with error
-      if (err){
-        var error = {status:'ERROR', message: 'Error updating animal'};
-        return res.json(error);
-      }
-
-      console.log('updated the animal!');
-      console.log(data);
-
-      // now return the json data of the new person
-      var jsonData = {
-        status: 'OK',
-        animal: data
-      }
-
-      return res.json(jsonData);
-
-    })
-
-})
-
-/**
- * GET '/api/delete/:id'
- * Receives a GET request specifying the animal to delete
- * @param  {String} req.param('id'). The animalId
- * @return {Object} JSON
- */
-
-router.get('/api/delete/:id', function(req, res){
-
-  var requestedId = req.param('id');
-
-  // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
-  Animal.findByIdAndRemove(requestedId,function(err, data){
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal to delete'};
-      return res.json(error);
-    }
-
-    // otherwise, respond back with success
-    var jsonData = {
-      status: 'OK',
-      message: 'Successfully deleted id ' + requestedId
-    }
-
-    res.json(jsonData);
-
-  })
-
-})
 
 module.exports = router;
